ee9078a76dcca618e32a9cb15d03a83bf7f2d152
李应 <2439175930@qq.com>
2022-08-20 14:56:31 +0800

    安装less模块引入less module 样式

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const withLess = require('next-with-less')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -10,4 +13,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withLess(nextConfig)
--- pages/blog/editor/index.tsx
+++ pages/blog/editor/index.tsx
@@ -1,7 +1,9 @@
+import { Button, Checkbox, Form, Input } from 'antd'
+import React from 'react'
 import { EditorState } from 'braft-editor'
 import { NextPage } from 'next'
 import dynamic from 'next/dynamic'
-
+import styles from './editor.module.less'
 const Editor = ({ handleOnSubmitSaveContent }: EditorState) => {
   // 在next.js中直接调用Editor会报window undefined
   const Editor = dynamic(() => import('../../../components/Editor/index'), {
@@ -15,10 +17,52 @@ const EditorPage: NextPage = () => {
     console.log('save text', text)
   }
 
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
   return (
-    <>
-      <Editor handleOnSubmitSaveContent={handleOnSubmitSaveContent} />
-    </>
+    <div className={styles.editor}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="标题："
+          name=" title"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="时间"
+          name="date"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="作者"
+          name="author"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Editor handleOnSubmitSaveContent={handleOnSubmitSaveContent} />
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            发布文章
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
 
